7bd9026e05f91b19ab80b12b4e9c0cc92f6dde32
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-03-25 09:29:34 -0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -147,7 +147,7 @@ console.log(`-- Sheduled update E-Com Plus tokens '${cron}'`)
 
 const checkCart = require('./lib/cron-check-cart')
 exports.checkCart = functions.runWith({ timeoutSeconds: 360 })
-  .pubsub.schedule('*/4 * * * *').onRun(() => {
+  .pubsub.schedule('*/2 * * * *').onRun(() => {
     return prepareAppSdk().then(appSdk => {
       checkCart({ appSdk })
     })
